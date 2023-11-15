713dd1994b2a88c7b09eeb9a666f0d0d23f3d1f5
SeelyksDev <kevincordier.dev@gmail.com>
2023-11-15 21:50:52 +0100

    Filtres fonctionnels

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,21 +1,59 @@
 async function fetchTravaux() {
-const response = await fetch("http://localhost:5678/api/works");
-const travaux = await response.json();
-for (i = 0; i < travaux.length; i++){
-    const gallery = document.querySelector('.gallery')
-    let galleryHtml = `<figure>
-                      <img
-                      src="${travaux[i].imageUrl}"
-                      alt="${travaux[i].title}"/>
-                     <figcaption>${travaux[i].title}</figcaption>
-                     </figure>`
-    gallery.innerHTML += galleryHtml
-}
+    const response = await fetch("http://localhost:5678/api/works");
+    travauxData = await response.json();
+    displayTravaux(travauxData);
 }
 
-fetchTravaux()
+function displayTravaux(travaux) {
+    const gallery = document.querySelector('.gallery');
+    let galleryHtml = '';
+    travaux.forEach(travail => {
+        galleryHtml += `
+            <figure>
+                <img src="${travail.imageUrl}" alt="${travail.title}" />
+                <figcaption>${travail.title}</figcaption>
+            </figure>`;
+    });
+    gallery.innerHTML = galleryHtml;
+}
 
+function filtreTous() {
+    const btnTous = document.querySelector('.btn-tous')
+    btnTous.addEventListener('click', () => {
+        fetchTravaux()
+        displayTravaux(travauxData)
+    })
+}
 
+function filtreObjet() {
+    const btnObjet = document.querySelector('.btn-objets');
+    btnObjet.addEventListener('click', () => {
+        const objetFiltrees = travauxData.filter(travail => travail.category.name === "Objets");
+        displayTravaux(objetFiltrees);
+    });
+}
+
+function filtreAppartements() {
+    const btnAppartements = document.querySelector('.btn-appartements')
+    btnAppartements.addEventListener('click', () => {
+        const appartementsFiltrees = travauxData.filter(travail => travail.category.name === "Appartements")
+        displayTravaux(appartementsFiltrees)
+    })
+}
+
+function filtreHotelEtAppart() {
+    const btnHotelEtAppart = document.querySelector('.btn-hotels')
+    btnHotelEtAppart.addEventListener('click', () => {
+    const hotelEtAppartFiltees = travauxData.filter(travail => travail.category.name === "Hotels & restaurants" )
+    displayTravaux(hotelEtAppartFiltees)
+})
+}
+
+fetchTravaux();
+filtreTous()
+filtreObjet();
+filtreAppartements()
+filtreHotelEtAppart()
 
 
 
