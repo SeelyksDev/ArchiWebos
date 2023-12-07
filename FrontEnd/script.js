00c3da7f5e791e00213924e1fb72555bad7565c6
SeelyksDev <kevincordier.dev@gmail.com>
2023-12-07 13:35:15 +0100

    Essaie pour supprimer les travaux

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -17,40 +17,40 @@ const addBtn = document.createElement("button");
 let travauxData = "";
 
 const fetchTravaux = async (callback) => {
-    const response = await fetch("http://localhost:5678/api/works");
-    travauxData = await response.json();
-    callback(travauxData);
+  const response = await fetch("http://localhost:5678/api/works");
+  travauxData = await response.json();
+  callback(travauxData);
 };
 
 const displayTravaux = (travaux) => {
-    const gallery = document.querySelector(".gallery");
-    let galleryHtml = "";
-    travaux.forEach((travail) => {
-        galleryHtml += `
+  const gallery = document.querySelector(".gallery");
+  let galleryHtml = "";
+  travaux.forEach((travail) => {
+    galleryHtml += `
             <figure>
                 <img src="${travail.imageUrl}" alt="${travail.title}" />
                 <figcaption>${travail.title}</figcaption>
             </figure>`;
-    });
-    gallery.innerHTML = galleryHtml;
+  });
+  gallery.innerHTML = galleryHtml;
 };
 
 const choixCategorie = (categorie) => {
-    if (categorie === "") {
-        displayTravaux(travauxData);
-    } else {
-        const categorieFiltree = travauxData.filter(
-            (travail) => travail.category.name === categorie
-        );
-        displayTravaux(categorieFiltree);
-    }
+  if (categorie === "") {
+    displayTravaux(travauxData);
+  } else {
+    const categorieFiltree = travauxData.filter(
+      (travail) => travail.category.name === categorie
+    );
+    displayTravaux(categorieFiltree);
+  }
 };
 
 const eventListener = (categorie, btnSelector) => {
-    const btn = document.querySelector(btnSelector);
-    btn.addEventListener("click", (event) => {
-        choixCategorie(categorie);
-    });
+  const btn = document.querySelector(btnSelector);
+  btn.addEventListener("click", (event) => {
+    choixCategorie(categorie);
+  });
 };
 
 fetchTravaux(displayTravaux);
@@ -60,88 +60,111 @@ eventListener("Hotels & restaurants", ".btn-hotels");
 eventListener("", ".btn-tous");
 
 const modeAdmin = () => {
-    editionBox.style.height = "59px";
-    liLogin.style.display = "none";
-    liLogout.style.display = "block";
-    editionProjet.style.display = "block";
-    filterBox.style.visibility = "hidden";
+  editionBox.style.height = "59px";
+  liLogin.style.display = "none";
+  liLogout.style.display = "block";
+  editionProjet.style.display = "block";
+  filterBox.style.visibility = "hidden";
 };
 
 if (localStorage.getItem("Token")) {
-    modeAdmin();
-    btnLogout.addEventListener("click", () => {
-        window.localStorage.removeItem("Token");
-        window.location.href = "index.html";
-    });
+  modeAdmin();
+  btnLogout.addEventListener("click", () => {
+    window.localStorage.removeItem("Token");
+    window.location.href = "index.html";
+  });
 }
+
+let token = localStorage.getItem("Token");
 
 let modifBtnClicked = false;
 btnModif.addEventListener("click", () => {
-    displayModale();
-    if (modifBtnClicked === false) {
-        fetchTravaux(displayGallery);
-        modifBtnClicked = true;
-    }
+  displayModale();
+  if (modifBtnClicked === false) {
+    fetchTravaux(displayGallery);
+    modifBtnClicked = true;
+  }
 
-    containerModale.addEventListener("click", () => {
-        containerModale.style.display = "none";
-    });
+  containerModale.addEventListener("click", () => {
+    containerModale.style.display = "none";
+  });
 
-    document.querySelector(".modale").addEventListener("click", (event) => {
-        event.stopPropagation();
-    });
+  document.querySelector(".modale").addEventListener("click", (event) => {
+    event.stopPropagation();
+  });
 });
 
 cross.addEventListener("click", () => {
-    containerModale.style.display = "none";
+  containerModale.style.display = "none";
 });
 
 const displayGallery = (travaux) => {
-    travaux.forEach((travail) => {
-        console.log(travail);
-        const boxImg = document.createElement("div");
-        boxImg.classList.add("box-img");
-        const img = document.createElement("img");
-        img.src = travail.imageUrl;
-        img.classList.add("modale-img");
-        modifGallery.appendChild(boxImg);
-        boxImg.appendChild(img);
-        const btnRemove = document.createElement("button");
-        btnRemove.classList.add("remove-box");
-        btnRemove.dataset.id = travail.id;
-        btnRemove.addEventListener("click", (event) => {
-            console.log(event);
-        });
-
-        boxImg.appendChild(btnRemove);
-        const iconRemove = document.createElement("i");
-        iconRemove.classList.add("fa-solid", "fa-trash-can", "icon-remove");
-        btnRemove.appendChild(iconRemove);
+  travaux.forEach((travail) => {
+    console.log(travail);
+    const boxImg = document.createElement("div");
+    boxImg.classList.add("box-img");
+    const img = document.createElement("img");
+    img.src = travail.imageUrl;
+    img.classList.add("modale-img");
+    modifGallery.appendChild(boxImg);
+    boxImg.appendChild(img);
+    const btnRemove = document.createElement("button");
+    btnRemove.dataset.id = travail.id;
+    btnRemove.addEventListener("click", () => {
+      deleteTravaux(btnRemove.dataset.id);
     });
+    btnRemove.classList.add("remove-box");
+    btnRemove.dataset.id = travail.id;
+    btnRemove.addEventListener("click", (event) => {
+      console.log(event);
+    });
+
+    boxImg.appendChild(btnRemove);
+    const iconRemove = document.createElement("i");
+    iconRemove.classList.add("fa-solid", "fa-trash-can", "icon-remove");
+    btnRemove.appendChild(iconRemove);
+  });
 };
 
 const displayModale = () => {
-    containerModale.classList.add("modale-container");
-    body.appendChild(containerModale);
-    containerModale.style.display = "block";
+  containerModale.classList.add("modale-container");
+  body.appendChild(containerModale);
+  containerModale.style.display = "block";
 
-    modale.classList.add("modale");
-    containerModale.appendChild(modale);
+  modale.classList.add("modale");
+  containerModale.appendChild(modale);
 
-    cross.classList.add("fa-solid", "fa-xmark", "cross");
-    modale.appendChild(cross);
+  cross.classList.add("fa-solid", "fa-xmark", "cross");
+  modale.appendChild(cross);
 
-    alignCenterModale.classList.add("modale-aligncenter");
-    modale.appendChild(alignCenterModale);
+  alignCenterModale.classList.add("modale-aligncenter");
+  modale.appendChild(alignCenterModale);
 
-    modaleTitle.classList.add("modale-title");
-    modaleTitle.textContent = "Galerie photo";
-    alignCenterModale.appendChild(modaleTitle);
+  modaleTitle.classList.add("modale-title");
+  modaleTitle.textContent = "Galerie photo";
+  alignCenterModale.appendChild(modaleTitle);
 
-    modifGallery.classList.add("gallery-modif");
-    alignCenterModale.appendChild(modifGallery);
+  modifGallery.classList.add("gallery-modif");
+  alignCenterModale.appendChild(modifGallery);
 
-    addBtn.classList.add("add-btn");
-    addBtn.textContent = "Ajouter une photo";
-    alignCenterModale.appendChild(addBtn);
+  addBtn.classList.add("add-btn");
+  addBtn.textContent = "Ajouter une photo";
+  alignCenterModale.appendChild(addBtn);
 };
+
+const deleteTravaux = async (id) => {
+  let url = `http://localhost:5678/api/works/${id}`;
+  let options = {
+    method: "DELETE",
+    headers: {
+      Authorization: ` ${token}`,
+    },
+  };
+  let response = await fetch(url, options);
+  if (!response.ok) {
+    throw new Error("La requête n'a pas abouti");
+  } else {
+    console.log("reussi !");
+  }
+};
+console.log(token);
